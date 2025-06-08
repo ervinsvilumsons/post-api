ace46268891d23887c42bca5a8522cb9759949ee
ervinsvilumsons <ervins.vilumsons@gmail.com>
2025-06-08 17:24:10 +0300

    Fix CategorySeeder

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -23,7 +23,7 @@ class CategorySeeder extends Seeder
             ];
 
             foreach($categories as $category) {
-                Category::factory(['title' => $category])->create();
+                Category::factory()->create(['title' => $category]);
             }
         }
     }
